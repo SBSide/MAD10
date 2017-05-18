a0d84d732bf7dac52baaa94c49dcae33dd3286fb
Side <ivecis@live.co.kr>
2017-05-18 16:51:09 +0900

    init

--- app/src/main/java/com/example/iveci/mad10/MainActivity.java
+++ app/src/main/java/com/example/iveci/mad10/MainActivity.java
@@ -1,13 +1,122 @@
 package com.example.iveci.mad10;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyCanvas myCanvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("MY CANVAS.");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myCanvas = (MyCanvas) findViewById(R.id.canv);
+        int permissioninfo = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissioninfo == PackageManager.PERMISSION_GRANTED) {
+            File file = new File(getExternalPath() + "canvas");
+            if (!file.exists()) {
+                file.mkdir();
+                String msg = "디렉터리 생성";
+                if (file.isDirectory() == false) msg = "디렉터리 생성 오류";
+                Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+            }
+        }
+        else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                Toast.makeText(getApplicationContext(),
+                        "SDCard 쓰기 권한이 필요합니다. \n" + "설정에서 수동으로 활성화해주세요.",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"Blurring");
+        menu.findItem(1)
+                .setCheckable(true);
+        menu.add(0,2,1,"Coloring");
+        menu.findItem(2)
+                .setCheckable(true);
+        menu.add(0,3,2,"Pen Width Big");
+        menu.findItem(3)
+                .setCheckable(true);
+        menu.add(0,4,3,"Pen Color BLUE");
+        menu.add(0,5,4,"Pen Color GREEN");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == 1){
+            myCanvas.setOptType("BLUR");
+            item.setChecked(!item.isChecked());
+        }
+        else if(item.getItemId() == 2){
+            myCanvas.setOptType("COLOR");
+            item.setChecked(!item.isChecked());
+        }
+        else if(item.getItemId() == 3){
+            myCanvas.setOptType("BIG");
+            item.setChecked(!item.isChecked());
+        }
+        else if(item.getItemId() == 4){
+            myCanvas.setOptType("REDD");
+        }
+        else if(item.getItemId() == 5){
+            myCanvas.setOptType("BLUE");
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public String getExternalPath(){
+        String sdPath ="";
+        String ext = Environment.getExternalStorageState();
+        if(ext.equals(Environment.MEDIA_MOUNTED)){
+            sdPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/";
+        }
+        else sdPath = getFilesDir() + "";
+        return sdPath;
+    }
+
+    public void onClick(View v){
+        if(v.getId() == R.id.save){
+            final EditText text = new EditText(this);
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle("저장")
+                    .setMessage("저장할 이름을 입력 후, 저장하세요.")
+                    .setView(text)
+                    .setPositiveButton("저장", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (myCanvas.Save(getExternalPath() + "canvas/"+
+                                    text.getText().toString() + ".jpg"))
+                                Toast.makeText(getApplicationContext(),
+                                        "저장에 성공했습니다.",Toast.LENGTH_SHORT).show();
+                            else Toast.makeText(getApplicationContext(),
+                                    "저장에 실패했습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .setNegativeButton("취소", null).show();
+        }
+        else myCanvas.setOptType((String) v.getTag());
     }
 }
--- app/src/main/java/com/example/iveci/mad10/MyCanvas.java
+++ app/src/main/java/com/example/iveci/mad10/MyCanvas.java
@@ -1,34 +1,144 @@
 package com.example.iveci.mad10;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by iveci on 2017-05-18.
  */
 
 public class MyCanvas extends View {
+    Bitmap mBitmap;
+    Canvas mCanvas;
+    Paint mPaint = new Paint();
+    String opt = "";
     public MyCanvas(Context context) {
         super(context);
+        this.setLayerType(LAYER_TYPE_SOFTWARE, null);
+        mPaint.setColor(Color.CYAN);
     }
 
     public MyCanvas(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mPaint.setColor(Color.CYAN);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+
+        super.onSizeChanged(w, h, oldw, oldh);
+        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas();
+
+        mCanvas.setBitmap(mBitmap);
+        mCanvas.drawColor(Color.YELLOW);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.YELLOW);
 
-        canvas.drawRect(10, 10, 100, 100, paint);
+        Bitmap img = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        Bitmap bigImg = Bitmap.createScaledBitmap(img,img.getWidth()*2, img.getHeight()*2,false);
+        int cenX = (canvas.getWidth() - bigImg.getWidth())/2;
+        int cenY = (canvas.getHeight() - bigImg.getHeight())/2;
+        if(opt.equals("rotate"))
+            canvas.rotate(45,this.getWidth()/2, this.getHeight()/2);
+        else if (opt.equals("move"))
+            ;
+        else if (opt.equals("scale"))
+            ;
+        else if (opt.equals("skew"))
+            ;
+        else if (opt.equals("erase"))
+            mBitmap.eraseColor(Color.WHITE);
+        else if (opt.equals("open"))
+            ;
+        invalidate();
+        BlurMaskFilter blur = new BlurMaskFilter(100, BlurMaskFilter.Blur.NORMAL);
+        paint.setMaskFilter(blur);
+        canvas.drawBitmap(bigImg, cenX, cenY, paint);
+        /*        canvas.drawBitmap(img, 300, 350, paint);
+        Bitmap smallBitmap = Bitmap.createScaledBitmap(img,img.getWidth()/2, img.getHeight()/2,false);
+        canvas.drawBitmap(smallBitmap, 400, 350, paint);
 
-//        paint.setColor(Color.MAGENTA);
+        Bitmap bigBitmap = Bitmap.createScaledBitmap(img,img.getWidth()*2, img.getHeight()*2,false);
+        canvas.drawBitmap(bigBitmap, 100, 100, paint);
+        img.recycle();*/
+        float[] array = {
+                1, 0, 0, 0, -25f,
+                0, 1, 0, 0, -25f,
+                0, 0, 1, 0, -25f,
+                0, 0, 0, 2, 0
+        };
+/*
+        ColorMatrix colorMatrix = new ColorMatrix(array);
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+        paint.setColorFilter(filter);
+        canvas.drawBitmap(bigImg, cenX, cenY, paint);*/
+
+        if(stopX != -1 && stopY != -1) canvas.drawBitmap(img, stopX, stopY, paint);
+
+    }
+
+    float startX = -1, startY = -1, stopX = -1, stopY = -1;
+
+    public void setOptType(String opt){
+        this.opt = opt;
+    }
+
+    public boolean Save(String file_name) {
+        try {
+            FileOutputStream out = new FileOutputStream(file_name);
+            mBitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+            return true;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            startX = event.getX(); startY = event.getY();
+        }
+        else if(event.getAction() == MotionEvent.ACTION_MOVE){
+
+        }
+        else if(event.getAction() == MotionEvent.ACTION_UP){
+            stopX = event.getX(); stopY = event.getY();
+            invalidate();
+        }
+        return true;
     }
 }
